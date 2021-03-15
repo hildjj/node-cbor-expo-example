c7674c91d0d151c9e26af57ce1e460dc3b7ef0cd
Joe Hildebrand <joe-github@cursive.net>
2021-03-15 15:20:04 -0600

    Make cbor work

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import process from 'process';
+global.process = process;
+import cbor from 'cbor';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, Expo: {cbor.encodeOne('Hello, Expo').toString('hex')}</Text>
       <StatusBar style="auto" />
     </View>
   );
--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,9 @@ module.exports = {
   transformer: {
     assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   },
+  resolver: {
+    extraNodeModules: {
+      stream: require.resolve('stream-browserify'),
+    },
+  },
 };
